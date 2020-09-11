75bef577066a06eb5ac96d7993f252ae9e386194
M-Alaa96 <70509609+M-Alaa96@users.noreply.github.com>
2020-09-11 17:43:41 +0200

    Update Networks.java

--- src/transport/protocol/Networks.java
+++ src/transport/protocol/Networks.java
@@ -1,15 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package transport.protocol;
 
-/**
- *
- * @author Rowan Salem
- */
 public abstract class Networks implements Runnable {
 
     protected static Mode mode;
